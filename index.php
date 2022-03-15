5f4e4e2c98310b35e07f0f463554ec5c92a6fb0d
hazira-rozi <hazirafakhrurrozi@hotmail.com>
2022-03-15 11:47:50 +0700

    Fitur Tambahan

--- index.php
+++ index.php
@@ -29,14 +29,28 @@
         <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-12 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
-                <br/><h1 class="h3 text-gray-900 mb-2 text-center">Selamat datang di catatan Kegiatan</h1>
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h5 text-gray-900 mb-1">Masukkan NISN dan Nama Lengkap</h1>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <br/>
+                            <h1 class="h3 text-gray-900 text-center">Selamat datang di catatan
+                                Kegiatan</h1>
+                                <br/>
+                            <div class="card-body p-0">
+                                <!-- Nested Row within Card Body -->
+                                <div class="row">
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        <img src="img/logo_rpl.png" alt="Responsive image" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h5 text-gray-900 mb-2">Masukkan NISN dan Nama Lengkap</h1>
                                     </div>
                                     <form class="user" method="post" action="do_login.php">
                                         <div class="form-group">
@@ -47,13 +61,6 @@
                                         <div class="form-group">
                                             <input type="text" name="nama_lengkap" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Masukkan nama lengkap anda">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             <i class="fa fa-key"></i>
